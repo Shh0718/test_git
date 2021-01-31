e5a4c076bcd187e837b3b35dc47382e0d0e2292d
calmdown-123 <18943659276@189.cn>
2021-01-31 22:34:40 +0800

    add py

---
+++ together.py
@@ -0,0 +1,10 @@
+import datetime
+
+#now = datetime.datetime.now().strftime("%Y-%m-%d")
+current_year = datetime.datetime.now().strftime("%Y")
+current_month = datetime.datetime.now().strftime("%m")
+current_day = datetime.datetime.now().strftime("%d")
+
+d1 = datetime.date(int(current_year),int(current_month),int(current_day))
+d2 = datetime.date(2020,7,18)
+print("We've been together for {} days !!!".format((d1-d2).days+1))

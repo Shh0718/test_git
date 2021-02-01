e03104ad607a7d0d1750c94232cac923afefdbdb
calmdown-123 <78247302+calmdown-123@users.noreply.github.com>
2021-02-01 20:48:48 +0800

    Update together.py

--- together.py
+++ together.py
@@ -7,4 +7,6 @@ current_day = datetime.datetime.now().strftime("%d")
 
 d1 = datetime.date(int(current_year),int(current_month),int(current_day))
 d2 = datetime.date(2020,7,18)
+d3 = datetime.date(2020,6,8)
 print("We've been together for {} days !!!".format((d1-d2).days+1))
+print("We've been kown each other for {} days !!!".format((d1-d3).days+1))
